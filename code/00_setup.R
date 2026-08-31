ded29d8be5ce5ac33eeee3126411ac1ba2dc8269
Faustina Awuntayami <famaawuntayami@gmail.com>
2026-08-31 02:44:13 +0000

    Initial research project setup

---
+++ code/00_setup.R
@@ -0,0 +1,13 @@
+dir.create("data")
+dir.create("data/raw")
+dir.create("data/processed")
+
+dir.create("code")
+
+dir.create("outputs")
+dir.create("outputs/figures")
+dir.create("outputs/tables")
+
+dir.create("literature")
+
+dir.create("paper")

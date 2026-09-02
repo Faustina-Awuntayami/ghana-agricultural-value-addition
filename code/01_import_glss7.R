e92fa4fa963e4399acf50e3076ad04fcf7fb9ad8
Faustina Awuntayami <famaawuntayami@gmail.com>
2026-09-02 17:34:44 +0000

    import updates

--- code/01_import_glss7.R
+++ code/01_import_glss7.R
@@ -1,35 +1,69 @@
+#-------------------------------------------------------
+# current file: 01_import_glss7.R 
+# purpose: is to *import* all the glss7 dataset
+#-------------------------------------------------------
+# loading important packages
+#-------------------------------------------------------
+# package for cleaning, transforming and visualizing data
 library(tidyverse)
+
+#package for reading Stata (.dta files)
 library(haven)
+
+#package for cleaning variable names and inspecting datasets easily
 library(janitor)
 
-# Folder containing GLSS Part A files
-partA_path <- "data/raw/g7PartA_upd"
+# -------------------------------------------------------------
+# where to find relevant dataset to this study
+# -------------------------------------------------------------
+# 1. part A of the dataset is found in the data/raw/g7PartA_upd
+ds_pathA <- "data/raw/g7PartA_upd"
 
-# List all .dta files in Part A
+# list all the .dta files in this folder and assign it to partA_files
 partA_files <- list.files(
-  partA_path,
+  path = ds_pathA,
   pattern = "\\.dta$",
   full.names = TRUE
 )
-
-# Print filenames
+# list file names without the folder path
 basename(partA_files)
 
-# List only Stata files anywhere inside data/raw
-list.files("data/raw", pattern = "\\.dta$", recursive = TRUE)
-# ----------------------------------------------------
-# List only the agriculture files in Part B
-list.files("data/raw/g7PartB", pattern = "g7sec8", full.names = FALSE)
-# Import the processing dataset from Part B
-glss8g <- read_dta("data/raw/g7PartB/g7sec8g.dta")
+# check my current working dir
+getwd()
 
-# Number of rows and columns
-dim(glss8g)
+# G7sec8g.dta	contains data on Processing Agricultural Produce: most important file for this research
+# its located in data/raw/g7PartB/g7sec8g.dta
+# import it into r and assign it to partB_sec8g
+partB_sec8g <- read_dta("data/raw/g7PartB/g7sec8g.dta")
 
-# First six rows
-head(glss8g)
+# lets see what it contains
+dim(partB_sec8g) # size: it contains 16844 rows (households) and 28 columns
+head(partB_sec8g) # view first few rows
+names(partB_sec8g) # names of all the variables (household ID, etc)
+glimpse(partB_sec8g) # view the strcuture of all the variables including data types, sample values, etc
 
-# Overview of variables
-glimpse(glss8g)
-# Show all variable names
-names(glss8g)
+# since most variable names start with code names like s8g (section 8 part g),
+# lets look for all of them
+names(partB_sec8g) |> str_subset("s8g") # |> assigns whats on the left to the right unlike <-
+
+# now lets see the descriptions of the s8g variable names
+# load package to do that
+install.packages("labelled")
+library(labelled)
+# now give the human understandable desc of the s8g variable names
+look_for(partB_sec8g)
+
+# view the dataset as a table
+view(partB_sec8g)
+
+# look for missing values. nb: structural missing values occur based on answers to previous questions
+partB_sec8g |>
+  summarise(across(everything(), ~sum(is.na(.))))
+
+#-----------------------------relating to research questions------------
+# how many households processed agricultural products
+partB_sec8g |>
+  count(s8gq1)
+
+#
+val_labels(partB_sec8g$s8gq1)
